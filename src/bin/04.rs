08424a268a0e915b408882cdfcbdafc3bab680ae
jwl <jwl@cloverwall.net>
2025-12-03 21:33:14 -0800

    tests

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,10 +1,12 @@
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    println!("input is {input}");
     None
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
+    println!("input is {input}");
     None
 }
 
@@ -15,7 +17,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]
