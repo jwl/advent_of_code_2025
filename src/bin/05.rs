d3e21ad4fc404495d9fb014db2972d572b8ce590
jwl <jwl@cloverwall.net>
2025-12-05 07:08:49 -0800

    WIP

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,6 +1,13 @@
 advent_of_code::solution!(5);
 
+pub fn populate_fresh_map(fresh_ranges: &str) -> HashMap5
+
 pub fn part_one(input: &str) -> Option<u64> {
+    // Parse input into fresh ranges, and then ingredient IDs
+    let mut input_parts = input.split("\n\n");
+
+    let fresh_ranges = input_parts.next().unwrap_or("").to_string();
+    let ingredient_ids = input_parts.next().unwrap_or("").to_string();
     None
 }
 
@@ -15,7 +22,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
